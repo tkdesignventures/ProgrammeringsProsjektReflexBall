def28e160450ce0181b8be7669df68c8b93bf166
Peterbys <tore.kanstad@gmail.com>
2015-06-12 16:20:22 +0200

    updated updateGame()

--- refball.c
+++ refball.c
@@ -18,21 +18,26 @@ void moveBall(){
 
 void moveStriker(unsigned char direction){
 	
-	if((striker.x - STRIKER_WIDTH >= L_EDGE_COORD) && (striker.x+STRIKER_WIDTH <= R_EDGE_COORD));
-	if(direction)
-		striker.x +=1;
-	else
-		striker.x -=1;
-}
+	if((striker->x - STRIKER_WIDTH >= L_EDGE_COORD) && (striker->x+STRIKER_WIDTH <= R_EDGE_COORD)){
+			if(direction)
+				striker->x +=1;
+			else
+				striker->x -=1;
+		}
+	}
+
 char isDead()
 	return lives >=0;
 }
 
-void updateGame(){
+void updateGame()
 	if(gameActive && getCentis()- gameSpeed > gameTime )
 	{
-			
-		
+		drawStriker(striker->xlast,7)
+		striker->xlast = striker->x;
+		drawStriker(striker->x,0);
+		moveBall();
+		checkBall();			
 	}
 }
 
