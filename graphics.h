c2e87185169170de4b3960b6bd3cf7516c920afb
MartinBrunsgaard <mb.brunsgaard@gmail.com>
2015-06-12 16:25:26 +0200

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,9 @@
+#ifndef _GRAPHICS_H_
+#define _GRAPHICS_H_
+
+void drawBox(unsigned char x, unsigned char y,unsigned char color);
+void drawBall(unsigned char x, unsigned char y, unsigned char color);
+void drawStriker(unsigned char x,unsigned char color);
+void drawBounds(int x1,int y1, int x2, int y2);
+
+#endif
