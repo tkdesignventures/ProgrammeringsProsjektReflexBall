7c5c6c2bb7974615d6fbc59832fe95fa05e5ce70
Peterbys <tore.kanstad@gmail.com>
2015-06-12 16:50:05 +0200

    ctimer added

--- ctimer.c
+++ ctimer.c
@@ -1,1 +1,36 @@
+long time;
+unsigned int delay;
 
+void resetTimer(){
+time = 0;
+}
+
+timer0int(){
+time++;
+delay--;
+}
+
+int getDelay(){
+return delay;
+}
+
+void setDelay(int input){
+delay = input;
+}
+
+
+void setTimer(){
+	char preScale = 0x07<<3;	
+	DI();
+	T0CTL = 0x01 | preScale;
+	T0H = 0x00;
+	T0L = 0x01;
+	T0RH = 0x05;
+	T0RL = 0xA0;
+	SET_VECTOR(TIMER0, timer0int);
+	IRQ0ENH |= 0x20;
+	IRQ0ENL |= 0x20;
+	T0CTL |= 0x80;
+
+	EI();
+}
