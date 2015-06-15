6b096ec23458898415b90b01dbf3e02c3ac06bed
toreKan <s144021@student.dtu.dk>
2015-06-15 13:48:04 +0200

    Menu-filer
    ny versjon

--- menu.h
+++ menu.h
@@ -1,5 +1,11 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+#define LINE_NUMBER 15
 
+void printLogo();
 void initiateMenu();
 void moveMarker(char up);
 char getChoice();
+void printDifficulty();
 
+#endif
