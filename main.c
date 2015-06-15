f676e556241dee38c044ff84d133191d49375bc0
Peterbys <tore.kanstad@gmail.com>
2015-06-15 13:39:44 +0200

    f

--- main.c
+++ main.c
@@ -14,43 +14,49 @@ void main(){
 		unsigned char boxesLeft;
 		unsigned char waitStart;
 		unsigned long refreshTime;
+		unsigned char level;
+		unsigned char key;
 
-		waitStart = 0;
+		waitStart = 1;
 		gameTime = 0;
 		refreshTime=0;
 		gameActive = 1;
 		lives = 3;
-		boxesLeft = initGame();
+		boxes = initGame(&ball,&striker,&box,level);
 		setTimer();
 
 		for(;;)
 		{
+			key = getKey();
+			if(key == 1)
+			waitStart = 1;
+			else if(key == 2)
+				moveStriker(&striker, 1);
+			else if(key == 4)
+				moveStriker(&striker,0);
+
 			if(getCentis()- GAMESPEED >= refreshTime){
-				updateGame();
+					refreshTime = getCentis();
+					if(gameActive){
+							gameTime += GAMESPEED;
+							if(!waitStart){
+									drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),7);
+									moveBall(&ball);
+									drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),0);
+									if(!checkBall(&ball, &striker, &box,boxes){
+										lives --;
+										waitStart = 0;
+									}
+									}
+							else {
+									drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),7);
+									setBallOverStriker();
+									drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),0);
+									}
+
+						}
+
 			}
 
 		}
-
 }
-
-
-	long updateGame() {
-		if(getCentis()- GAMESPEED >= refreshTime )
-		{
-			refreshTime = getCentis();
-			if(gameActive){
-				gameTime += GAMESPEED;
-					if(!waitStart){
-							drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),7);
-							moveBall();
-							drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),0);
-							checkBall();
-							}
-					else {
-							drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),7);
-							setBallOverStriker();
-							drawBall(toTerminalCoords(ball.x),toTerminalCoords(ball.y),0);
-							}
-					}
-					return
-	}
