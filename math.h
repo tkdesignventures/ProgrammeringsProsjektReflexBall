2df430b407e88ff9c9330762eadea6ae028c9a70
MartinBrunsgaard <mb.brunsgaard@gmail.com>
2015-06-17 11:28:52 +0200

    Update math.h

--- math.h
+++ math.h
@@ -1,17 +1,16 @@
 #include "refball.h"
+#include "arcsin.h"
 #ifndef _MATH_H_
 #define _MATH_H_
 #define FIX14_SHIFT 14
 #define FIX14_MULT(a, b) ( (a) * (b) >> FIX14_SHIFT)
 #define FIX14_div(a,b)   ( ((a) << FIX14_SHIFT / (b) ))
 
-typedef struct {
-	unsigned char x,y;
-} TVector ;
+
 
 long sin(int x);
 long cos(int x);
-long angle(int x);
+int angle(int x);
 void rotate(Ball * ball, int ang);
 void initVector(TVector *v, long x, long y);
 long expand(long i);
