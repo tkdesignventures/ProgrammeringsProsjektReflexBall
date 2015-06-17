57057bbafd889b85472fff36fa7f07309a145130
MartinBrunsgaard <mb.brunsgaard@gmail.com>
2015-06-17 11:32:25 +0200

    Create arcsin.h

---
+++ arcsin.h
@@ -0,0 +1,17 @@
+// =====================================================================
+//
+//	Exported by Cearn's excellut v1.0
+//	(comments, kudos, flames to daytshen@hotmail.com)
+//
+// =====================================================================
+
+#ifndef ARCSIN_H
+#define ARCSIN_H
+
+// === LUT SIZES ===
+#define ARCSIN_SIZE 512
+
+// === LUT DECLARATIONS ===
+extern const signed short ARCSIN[512];
+
+#endif	// ARCSIN_H
