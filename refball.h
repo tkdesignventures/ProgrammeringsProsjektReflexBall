96f3e79edc5848f3d5ddf9dfc1dc3094b4117432
Peterbys <tore.kanstad@gmail.com>
2015-06-18 14:01:00 +0200

    Revert "gitty"
    This reverts commit 9958a63adefe6d4f208af826a78b6a0d8d4f6dc6.

--- refball.h
+++ refball.h
@@ -10,21 +10,13 @@
 #define STRIKER_Y  60
 #define STRIKER_START  20
 #define TOP_EDGE_COORD 2
-#define BLANK 0
-#define EDGE 180
-#define TOP 196
-#define STRIKER 220
   typedef struct{
   long x,y,xdir, ydir;
   unsigned char power, outOfBounds;
   } Ball;
-  typedef struct{
-    unsigned char *x, *y, * durability;
-    unsigned char size, capacity;
-  } Box;
 
   void moveBall(Ball * ball);
-  unsigned char moveStriker(long * x, char direction);
+  void moveStriker(long * x, char direction);
   void checkBall(Ball * ball, int x);
   long toTerminalCoordinates(long x);
   void setBallOverStriker( Ball * ball,  long st);
