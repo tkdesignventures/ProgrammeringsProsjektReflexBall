ded1675fad572db36dc99c1aecc327c9372ffb11
Martin <mb.brunsgaard@gmail.com>
2015-06-19 10:49:38 +0200

    OP

--- graphics.h
+++ graphics.h
@@ -6,7 +6,7 @@ void drawBox(unsigned char x, unsigned char y,unsigned char color);
 void drawBall(unsigned char x, unsigned char y, unsigned char color);
 void fixBall(unsigned char x, unsigned char y,char tegn);
 void drawStriker(unsigned char x,unsigned char color);
-void drawBounds(int x1,int y1, int x2, int y2);
+void drawBounds(int x1,int y1, int x2, int y2,unsigned char color);
 void moveDrawStriker(unsigned char x, unsigned char direction);
 void drawLogo();
 void drawGameOver();
